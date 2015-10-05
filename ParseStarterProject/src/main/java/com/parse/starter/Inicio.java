74950bea15c160142489ec49ae84cb462cee9fde
Rogger <rogger.aburto@gmail.com>
2015-10-05 14:45:05 -0500

    |

---
+++ ParseStarterProject/src/main/java/com/parse/starter/Inicio.java
@@ -0,0 +1,45 @@
+package com.parse.starter;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.LinkedList;
+
+
+public class Inicio extends ActionBarActivity  {
+
+    private String[] mNavigationDrawerItemTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+        mNavigationDrawerItemTitles= getResources().getStringArray(R.array.navigation_drawer_items_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_inicio, menu);
+        return true;
+    }
+
+
+}
